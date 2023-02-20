d73e29cb5b57b7bb9ca63b2b7aa954735f589061
刘勇 <lee.oblivion@outlook.com>
2023-02-21 00:46:50 +0800

    feat: 新增 语雀
    version:2.4.2.1017

---
+++ Casks/yuque.rb
@@ -0,0 +1,11 @@
+cask "yuque" do
+  version "2.4.2.1017"
+  sha256 "90c871a04a889dc6f1ae3ed4fd74dfc6aecd327df0a5f0afea3e666bf650b1b7"
+
+  url "https://app.nlark.com/yuque-desktop/#{version}/Yuque-#{version}.dmg"
+  name "语雀"
+  desc "Cloud knowledge base"
+  homepage "https://www.yuque.com/"
+
+  app "语雀.app"
+end
